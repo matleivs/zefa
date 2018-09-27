30ef9b4b996dbcc153e5580f7a00b75eaa9c9d9a
leilinikbin <nikbin@itemis.de>
2018-09-27 19:10:39 +0200

    Setup: Add Firebase Auth & FirebaseUI dependencies
    - implement Authentication with Firebase

--- ZeiterfassungClient/app/src/main/java/com/itemis/nikbin/zeiterfassungclient/MainActivity.java
+++ ZeiterfassungClient/app/src/main/java/com/itemis/nikbin/zeiterfassungclient/MainActivity.java
@@ -1,21 +1,27 @@
 package com.itemis.nikbin.zeiterfassungclient;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.functions.FirebaseFunctions;
 import com.google.firebase.functions.FirebaseFunctionsException;
 import com.google.firebase.functions.HttpsCallableResult;
 
 import java.text.SimpleDateFormat;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -29,6 +35,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     // Add workday views
     private EditText mCurrentStartTimeField;
     private Button mAddWorkdayButton;
+    private Button mSignInButton;
 
     // my instance of Firebase Cloud Functions
     private FirebaseFunctions mFunctions;
@@ -42,39 +49,68 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mAddWorkdayButton = findViewById(R.id.button_add_workday);
         mAddWorkdayButton.setOnClickListener(this);
 
+        mSignInButton = findViewById(R.id.button_sign_in);
+        mSignInButton.setOnClickListener(this);
 
         // [START initialize_functions_instance]
         mFunctions = FirebaseFunctions.getInstance();
         // [END initialize_functions_instance]
     }
 
+    private void onClickSignIn() {
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            showSnackbar("Signed in alright");
+            return;
+        }
+
+        signInWithFirebaseUiAuth();
+    }
+
+    private void signInWithFirebaseUiAuth() {
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(Collections.singletonList(
+                        new AuthUI.IdpConfig.EmailBuilder().build()))
+                .setIsSmartLockEnabled(false)
+                .build();
+
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                showSnackbar("Signed in yeah");
+            } else {
+                showSnackbar("Error signing in");
+
+                IdpResponse response = IdpResponse.fromResultIntent(data);
+//                FirebaseUiException error = response.getError();
+//                Log.w(TAG, "signInWithFirebaseUiAuth", error);
+                Log.d(TAG, "signInWithFirebaseUiAuth - Error");
+            }
+        }
+    }
+
+    private void showSnackbar(String message) {
+        Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_SHORT).show();
+    }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
+            case R.id.button_sign_in:
+                onClickSignIn();
+                break;
             case R.id.button_add_workday:
                 onClickAddWorkday();
                 break;
         }
     }
 
-
-    /**
-     * Date and Time Has to come from the Client side, as the server time could differ from local
-     */
-    private String getSimpleTime() {
-        SimpleDateFormat formatter = new SimpleDateFormat(
-                "HH:mm");
-        Date currentTime = new Date();
-        return (formatter.format(currentTime));
-    }
-
-    private String getSimpleDate() {
-        SimpleDateFormat formatter = new SimpleDateFormat(
-                "dd.MM.yyyy");
-        Date currentTime = new Date();
-        return (formatter.format(currentTime));
-    }
 
     private Task<String> addWorkday(String date, String startTime) {
         // Create the arguments to the Callable Function
@@ -97,6 +133,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void onClickAddWorkday() {
+        showSnackbar("yes u successfully clicked 1 button");
 
         String date = getSimpleDate();
         String time = getSimpleTime(); // using default current Time atm; later:add manual set time option
@@ -117,15 +154,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //                                Log.d(TAG, "addWorkday:onFailure Details: " + details.toString());
                             }
                             Log.w(TAG, "addWorkday:onFailure: " + e.getMessage(), e);
+                            showSnackbar("An error occurred.");
                             return;
                         }
 
                         // if task.isSuccessful:
                         String result = task.getResult();
                         Log.d(TAG, "addWorkday: successful");
+                        showSnackbar("Result: " + result);
                         mCurrentStartTimeField.setText(result);
                     }
                 });
         // [END call_addWorkday]
+    }
+
+
+    /**
+     * Taking Date and Time from the Client side, as the server time could differ from local
+     */
+    private String getSimpleTime() {
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
+        Date currentTime = new Date();
+        return (formatter.format(currentTime));
+    }
+
+    private String getSimpleDate() {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
+        Date currentTime = new Date();
+        return (formatter.format(currentTime));
     }
 }
